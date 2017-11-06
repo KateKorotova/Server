795b67b911ac2c91c172ec3a102a94f673243e62
katya_000 <katya_000@KATEKOROTOVA>
2017-11-06 21:43:53 +0200

    socket part

--- server/Server.cpp
+++ server/Server.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "Winsock2.h"
+#include <ws2tcpip.h>
+
+#include <string>
 
 
 #pragma comment(lib, "Ws2_32.lib")
@@ -32,6 +35,7 @@ int main(){
 	bind(listening, (sockaddr*)&hint, sizeof(hint));
 	
 	// tell Winsock the socket is for listening
+
 	listen(listening, SOMAXCONN);
 
 	// wait for connection 
@@ -41,8 +45,44 @@ int main(){
 
 	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
 
+	char host[NI_MAXHOST]; // client's remote name
+	char service[NI_MAXHOST]; // service (i.e. port) the client is connect to
 
+	ZeroMemory(service, NI_MAXSERV);
+	ZeroMemory(host, NI_MAXHOST);
 
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+		cout << host << " connected on port " << service << endl;
+	else {
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		cout << host << " connected on port " << ntohs(client.sin_port)<< endl;
 
+	}
 
+	// close listening socket
+	closesocket(listening);
+
+	// while loop: accept and echo message back to client
+	char buf[4096];
+	while (true) {
+		ZeroMemory(buf, 4096);
+		// wait for client to send data
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+		if (bytesReceived == SOCKET_ERROR) {
+			cerr << "Error in recv()" << endl;
+			break;
+		}
+		if (bytesReceived == 0) {
+			cout << "Client disconneced" << endl;
+			break;
+		}
+		// echo message back to client
+
+		send(clientSocket, buf, bytesReceived + 1, 0);
+	}
+	// close the socket 
+	closesocket(clientSocket);
+	//cleanup winsock
+	WSACleanup();
+	
 }
